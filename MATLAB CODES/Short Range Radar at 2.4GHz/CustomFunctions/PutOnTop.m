228a35ac5fd863de2aeb7e238458fbd8aef79288
Giandomenico Panettieri <57620194+GiandomenicoPanettieri@users.noreply.github.com>
2023-10-27 14:45:39 +0200

    Delete MATLAB CODES/Short Range Radar at 2.4GHz directory

--- MATLAB CODES/Short Range Radar at 2.4GHz/CustomFunctions/PutOnTop.m
+++
@@ -1,7 +0,0 @@
-function Mout= PutOnTop(M,V)
-%   Mout= PutOnTop(M,V) Puts matrix V on top of matrix M without changing the size of M
- 
-    M = [V ; M ];
-    M = M(1:end-length(V(:,1)),:);
-Mout=M;
-end
--- MATLAB CODES/Short Range Radar at 2.4GHz/README.txt
+++
@@ -1,4 +0,0 @@
-Make sure that $CustomFunction and $AudiosFiles folders are added in the path folder of Matlab.
-
-To have infos about custom functions uses
-help name_function
